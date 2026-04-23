f0393ad8b386d3c5bc4e74e42c80e67a065940d1
amstopup7 <amstopup7@gmail.com>
2026-04-23 22:30:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ def handle_requests():
             elif server_name in {"BR", "US", "SAC", "NA"}:
                 url = "https://client.us.freefiremobile.com/LikeProfile"
             else:
-                url = "clientbp.ggpolarbear.com/LikeProfile"
+                url = "https://clientbp.ggpolarbear.com/LikeProfile"
 
             asyncio.run(send_multiple_requests(uid, server_name, url))
 
